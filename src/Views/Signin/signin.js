1f517c45eae767968877ee960e3689cf11792107
ali2527 <ali.talha3357@gmail.com>
2022-02-15 22:23:36 +0500

    signin page started

--- src/Views/Signin/signin.js
+++ src/Views/Signin/signin.js
@@ -2,15 +2,31 @@
 import React from "react";
 
 //UI Library
-import { Layout } from "antd";
+import { Layout, Card, Row, Col } from "antd";
 
 //CSS
-import "./signin.css"
+import "./signin.css";
 
 function Signin() {
-  return <Layout className="SigninLayout">
-
-  </Layout>;
+  return (
+    <Layout className="SigninLayout">
+      <Row style={{ width: "100%" }} justify="center">
+        <Col span={14}>
+          <Card className="signinCard" bordered={false}>
+          <Row style={{ width: "100%" }} justify="center">
+          <Col span={12}>
+            <div style={{backgroundColor:'#502989', borderRadius:"20px",height:'74vh', display:"flex", justifyContent:'center', alignItems:'center '}}>
+            <h1>aple</h1>
+            </div>
+            
+          </Col>
+          <Col span={12}></Col>
+          </Row>
+          </Card>
+        </Col>
+      </Row>
+    </Layout>
+  );
 }
 
 export default Signin;
